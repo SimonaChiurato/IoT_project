e14dd6bcb7943bc4452747fa40de69732958fb59
Conve <s282881@studenti.polito.it>
2022-12-23 12:28:03 +0100

    modi

--- HomeCatalog/HomeCatalog.py
+++ HomeCatalog/HomeCatalog.py
@@ -1,12 +1,56 @@
 #service catalog
 import json
-import datetime
-import requests
-import cherrypy
-import ManagerHome
+
 #metti tutte le funzioni e metti il main nel manager home
-def getPort(self):
-  ]
+class HomeCatalog:
+  #non so se questo init vada messo due volte o meno, oppure come farlo funzionare mettendolo una volta sola
+  def __init__(self):
+    self.settings = "HomeCatalog_settings.json"
+    self.cat = json.load(open(self.settings))
+    # self.conf["resource_catalogs"]=[]
+    with open(self.settings, "w") as myfile:
+      myfile.write(json.dumps(self.cat))
 
 
-def getBrokerPort(self):
+  def returnAllPatients(self):
+    result = { 'names':[] }
+    for res in self.cat["resource_catalogs"]:
+      result['names'].append(res['patient'] +'\n')
+    return result
+
+  def searchPatient(self, params):
+    for entry in self.cat["resource_catalogs"]:
+      if (entry["patient"] == params["patient"]):
+        return entry
+
+  def newResCat(self, message):
+    already_existing = False
+    for entry in self.cat["resource_catalogs"]:
+      if entry["base_topic"] == message["base_topic"]:
+        already_existing = True
+    if already_existing == False:
+      self.cat["resource_catalogs"].append(message)
+      with open(self.cat, "w") as f:
+        json.dump(self.cat, f)
+      print ("Resource catalog inserted with success")
+    else:
+      print ("Resource catalog already existing")
+
+  def updateResCat(self,message):
+    already_existing = False
+    for entry in self.cat["resource_catalogs"]:  # controlla salvataggio e nome degli attributi
+      if entry["base_topic"] == message["base_topic"]:
+        already_existing = True
+        entry["broker"] = message["broker"]
+        entry["broker_port"] = message["broker_port"]
+        entry["ip_address"] = message["ip_address"]
+        entry["ip_port"] = message["ip_port"]
+
+    if already_existing == False:
+      self.cat["resource_catalogs"].append(message)
+      print ("Resource catalog not found. New resource catalog inserted with success")
+    else:
+      print ("Resource catalog modified with success")
+
+    return self.cat
+
--- HomeCatalog/ManagerHome.py
+++ HomeCatalog/ManagerHome.py
@@ -1,41 +1,40 @@
 #home catalog
 import json
 import cherrypy
+from HomeCatalog import *
 
-#probabilmente 5.1 server
-#controllare i json.dump
-#Creare le funzioni da mettere nell'home catalog invece di tenere utto nel get
 class ManagerHome(object):
     exposed = True
+    CM = HomeCatalog()
     # mqtt-info
-    #Usa i settings
-    broker = "test.mosquitto.org"
-    port = 1883
-    ip_address = "192.168.1.208"
-    ip_port = "8095"
-    res_cat = json.load(open("resource_catalog_info.json"))  # quale resource catalog dovrei aprire?
+    def __init__(self):
+        self.settings=json.load(open("HomeCatalog_settings.json"))
+        #self.cat=json.load(open(self.settings))
+        #self.conf["resource_catalogs"]=[]
+        #with open(self.settings, "w") as myfile:
+            #myfile.write(json.dumps(self.cat))
 
     def GET(self, *uri, **params):
 
         if len(uri) == 1:
 
             if uri[0] == 'resource_catalogs':           #restituisce tutti i res cat che ho
-                return json.dumps(self.res_cat)
+                return json.dumps(self.settings["resource_catalogs"])
 
             elif uri[0] == 'patients':     #restituisce la lista dei nomi dei pazienti
-                result = {"names": self.res_cat["patient"] + '\n' }
+                result = self.CM.returnAllPatients()
                 return json.dumps(result)
 
             elif uri[0] == 'info_room':     #restituisce le informazione della singola camera
-                results = [entry for entry in self.res_cat if (entry["patient"] == params["patient"])]   #verifica che funzioni scritto cosi
+                results = self.CM.searchPatient(params)
                 return json.dumps(results)  #controllare cosa restituisce quando la room non esiste, per il risultato sui sensori
 
             elif uri[0] == 'broker':
-                temp = {'broker': self.broker, 'port': self.port}
+                temp = {'broker': self.settings['broker'], 'port': self.settings['broker_port']}
                 return json.dumps(temp)
 
             elif uri[0] == 'base_topic':
-                return json.dumps(self.res_cat["base_topic"])
+                return json.dumps(self.settings["base_topic"])
         else:
             error_string = "incorrect URI or PARAMETERS URI" + {len(uri)} + "PAR" + {len(params)}           #cambia stringa
             raise cherrypy.HTTPError(400, error_string)
@@ -45,18 +44,7 @@ class ManagerHome(object):
             body = cherrypy.request.body.read()
             print("POST RECEIVED WITH BODY:", body)
             json_body = json.loads(body)
-            already_existing = False
-            for entry in self.res_cat:
-                if entry["base_topic"] == json_body["base_topic"]:
-                    already_existing = True
-            if already_existing == False:
-                self.res_cat.append(json_body)
-                print(json_body)
-                with open(self.res_cat, "w") as f:
-                    json.dump(self.res_cat, f)
-                return "Resource catalog inserted with success"
-            else:
-                return "Resource catalog already existing"
+            self.CM.newResCat(json_body)
         else:
             error_string = "incorrect URI or PARAMETERS" + {len(uri)}
             raise cherrypy.HTTPError(400, error_string)
@@ -67,25 +55,24 @@ class ManagerHome(object):
             body = cherrypy.request.body.read()
             print("PUT RECEIVED WITH BODY:", body)
             json_body = json.loads(body)
-            already_existing = False
-            for entry in self.res_cat:      #controlla salvataggio e nome degli attributi
-                if entry["base_topic"] == json_body["base_topic"]:
-                    already_existing = True
-                    entry["broker"] = json_body["broker"]
-                    entry["broker_port"] = json_body["broker_port"]
-                    entry["ip_address"] = json_body["ip_address"]
-                    entry["ip_port"] = json_body["ip_port"]
-                    print(json_body)
-                with open(self.res_cat, "w") as f:
-                    json.dump(self.res_cat, f)
-            if already_existing == False:
-                self.res_cat.append(json_body)
-                print(json_body)
-                with open(self.res_cat, "w") as f:
-                    json.dump(self.res_cat, f)
-                return "Resource catalog not found. New resource catalog inserted with success"
-            else:
-                return "Resource catalog modified with success"
+            result = self.CM.updateResCat(json_body)
+            with open(result, "w") as f:
+                json.dump(result, f)
         else:
             error_string = "incorrect URI or PARAMETERS" + {len(uri)}
             raise cherrypy.HTTPError(400, error_string)
+
+if __name__=="__main__":
+    service_settings=json.load(open("HomeCatalog_settings.json"))
+    conf={
+        '/': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.sessions.on': True,
+        }
+    }
+    cherrypy.tree.mount(ManagerHome(),'/', conf)
+    cherrypy.config.update(conf)
+    cherrypy.config.update({'server.socket_host':service_settings['ip_address_service']})
+    cherrypy.config.update({"server.socket_port":service_settings['ip_port']})  #qui loro hanno una funzione
+    cherrypy.engine.start()
+    cherrypy.engine.block()
